58d2707455d5e0881de25bd0c13bf3746220010c
mikerjacobi@gmail.com <mikerjacobi@gmail.com@f416ec4c-5df1-6d4c-783b-5653d8b2484f>
2011-06-02 05:28:41 +0000

    list of recipe names added

--- OzacobiRecipes/src/ozacobi/recipes/ozacobirecipes.java
+++ OzacobiRecipes/src/ozacobi/recipes/ozacobirecipes.java
@@ -1,14 +1,39 @@
 package ozacobi.recipes;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class ozacobirecipes extends Activity {
+public class ozacobirecipes extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        
+        String[] recipes = getResources().getStringArray(R.array.recipes_array);
+        setListAdapter((ListAdapter) new ArrayAdapter<String>(this, R.layout.list_item, recipes));
+        
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                int position, long id) {
+              // When clicked, show a toast with the TextView text
+              Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                  Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    
 }
